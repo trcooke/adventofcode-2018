fbdccfbf8be86636f285213e1745359bbc6221ea
Tim Cooke <tim@trcooke.co.uk>
2018-12-01 23:22:42 +0000

    Day one in Python. Easy enough, for now.

---
+++ 01/python/freq-changes.py
@@ -0,0 +1,23 @@
+changes = open("input")
+
+freqs = []
+for line in changes:
+    freqs.append(int(line))
+
+final_freq = 0
+for freq in freqs:
+    final_freq = final_freq + freq
+
+print final_freq
+
+seen = [0]
+final = 0
+keep_going = True
+while keep_going:
+    for freq in freqs:
+        final = final + freq
+        if final in seen:
+            print final
+            keep_going = False
+            break
+        seen.append(final)
