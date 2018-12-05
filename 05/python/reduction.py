e41087fdd1403017e21a44d5b56d164aac276542
Tim Cooke <tim@trcooke.co.uk>
2018-12-05 13:55:26 +0000

    Achieved a ~3x speed increase for Day 5 solution. Result!

--- 05/python/reduction.py
+++ 05/python/reduction.py
@@ -1,26 +1,30 @@
 from string import ascii_lowercase
 
-file = open("input-test")
+file = open("input")
 
-polymers = file.read().strip()
+polymers = list(file.read().strip())
 
 def react(polymers):
     more = True
+    start = 0
+    length = len(polymers)
     while more:
         more = False
-        for i, val in enumerate(polymers):
-            if i == 0:
-                continue
-            if (polymers[i-1].upper() == val.upper()) and (polymers[i-1].isupper() != val.isupper()):
-                polymers = polymers[0:i-1]+polymers[i+1:]
+        for i in range(start, length - 1):
+            if (polymers[i].upper() == polymers[i+1].upper()) and (polymers[i].isupper() != polymers[i+1].isupper()):
+                del polymers[i:i+2]
+                #polymers = polymers[0:i-1]+polymers[i+1:]
                 more = True
+                if i > 0:
+                    start = i - 1
+                length = length - 2
                 break
     return polymers
 
-#print len(react(polymers))
+print len(react(polymers))
 
 file = open("input")
-polymers = file.read().strip()
+polymers = list(file.read().strip())
 
 minlen = len(polymers)
 for c in ascii_lowercase:
